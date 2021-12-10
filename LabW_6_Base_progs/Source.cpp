e210f95dde25c9516a59398ead511cf823ff8629
Александр Немеров <nemerov_a@mail.ru>
2021-12-11 02:05:43 +0800

    готово

--- LabW_6_Base_progs/Source.cpp
+++ LabW_6_Base_progs/Source.cpp
@@ -1,93 +1,94 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
+#include <stdio.h>
+#include <conio.h>
 #include <time.h>
 #define N 10
-#define M 200
+#define M 1000
 using namespace std;
 
-int First(int arr[N], int sum_ch, int sum_nch) {
+int First() {
 	/*1) Написать функцию, которая возвращает 1, если количество четных
 	элементов одномерного массива больше количества нечетных. В противном
 	случае возвращает 0. Можно возвращать значение типа bool.*/
 
+	srand(time(0));
+	int arr[N]{ 0 }, sum_ch = 0, sum_nch = 0;
 
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < N; i++) {
 		arr[i] = rand() % 100;
-
-	for (int i = 0; i < N; i++)
-	{
 		if (arr[i] % 2 == 0) sum_ch += arr[i];
-		else sum_nch += arr[i];
+		if (arr[i] % 2 == 1) sum_nch += arr[i];
+		cout << arr[i] << " ";
 	}
 
-	for (int i = 0; i < N; i++)
-		cout << arr[i] << " ";
 	cout << endl;
+	cout << sum_ch<<"\t"<<sum_nch<<endl;
+	if (sum_ch > sum_nch) {
+		cout << "chet > ne_chet";
+			return 1;
+	}
 
-	if (sum_ch > sum_nch)
-		return 1;				//return 1
-	else
-		return 0;					// return 0;
-	//cout << endl << sum_ch << " " << sum_nch << endl;
+	else {
+		cout << "chet < ne_chet";
+			return 0;
+	}
 
+}
+
+void main1() {
+	setlocale(LC_ALL, "rus");
+	string str;
+	First();
 }
 
 
 
-int Second(int c, string str, string s, string n_str) {
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+void Second (char *str){
 	/*2) Написать функцию, которая принимает строку.В качестве результата возвращает
 	число символов в строке, которые повторяются(идут подряд).К примеру,
 	строка «б аааа ббб сс», результат работы функции – число 6 (а - 3, б – 2, с - 1).*/
 
+	char seps[] = " .,\t\n~!@#$%^&*():{}[]./-+<>?;", first('\0');
+	char *token = NULL;
 
-	cout << "Введите строку" << endl;
-	getline(cin, str);
-	c = str.length();
-	cout << endl;
-	str += " ";
+	int identical(1), len(0);
 
-	for (int i = 0; i < (c + 1); i++)
-	{
-		s = str[i];
-		if (s != " ")
-		{
-			n_str += str[i];
+	token = strtok(str, seps);
+
+	while (token != NULL) {
+		len=strlen(token);
+
+		for (int i = 0; i < len; i++) {
+			first = token[i];
+
+			if (first == token[i+1]) {
+				identical++;
+			}
+
+			else {
+				cout << first << " " << identical << endl;
+				identical = 1;
+			}
 		}
-		else
-		{
-			cout << n_str[0] << " " << n_str.length() << endl;
-			n_str = "\0";
-		}
+
+		token = strtok(NULL, seps);
 	}
-
 }
 
 
-int main() {
+
+void main() {
 	setlocale(LC_ALL, "rus");
-	srand(time(0));
-	int arr[N], sum_ch, sum_nch, b, c, v;
-	string str, s, n_str;
-
-	cout << "Work number (1 or 2): ";
-	cin >> v;
-
-	switch (v)
-	{
-	case(1):
-	{
-		First(0, 0, 0);
-		break;
-	}
-	case(2):
-	{
-		Second(0, '\0', '\0', '\0');
-		break;
-	}
-	default:
-	{
-		cout << "Error: invalid value";
-		break;
-	}
-	}
+	char str[M] = { "\0" };
+	cout << "Enter the text: ";
+	gets_s(str);
+	Second (str);
 }
